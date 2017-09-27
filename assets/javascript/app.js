ace6e8dfe17e9f6997ab4a2c050928510869c261
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-09-26 21:54:16 -0700

    Added links to html

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCTjLWY8RYA29tD2Z98hbARgDkIVoTKv3c",
+    authDomain: "project-train-scheduler.firebaseapp.com",
+    databaseURL: "https://project-train-scheduler.firebaseio.com",
+    projectId: "project-train-scheduler",
+    storageBucket: "project-train-scheduler.appspot.com",
+    messagingSenderId: "687439454592"
+  };
+  firebase.initializeApp(config);
