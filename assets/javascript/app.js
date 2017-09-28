aa1dce8f9adbd8cdf38d9f1c710cec74e4e2354f
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2017-09-27 17:41:09 -0700

    completed the scripts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,83 +11,69 @@
 
 	  	// Create a variable to reference the database.
     	var database = firebase.database();
-
-    	// Initial Values
-	    var name = "";
-	    var destination = "";
-	    var frequency = "5";
-	    var first = "10:30"
-	    //converting user input to be able to use in moment.js
-	    var firstMoment = moment(first, 'hh:mm');
-	    //converting the moment variable with same format as "now"
-	    var firstMoment2 = moment(firstMoment).format('hh:mm');
 	    
-	    
-	    // shows time in hours and min
-	    var now = moment().format('hh:mm');
-
-	    // calculates time until next bus
-	    var nowMin = moment.duration(now).asMinutes();
-	   	var firstMin = moment.duration(firstMoment2).asMinutes();
-	   	var nextArrival = (nowMin - firstMin) % frequency;
-	   	var minAway = frequency - nextArrival;
-	   	console.log(minAway);
-	   	
-	    // shows time to next bus
-	    var next = now.add(minAway, 'minutes');
-	    
-
-	    console.log(next);
-    	
-
 	    // Capture Button Click
-    $("#add-user").on("click", function(event) {
+      $("#add-user").on("click", function(event) {
       event.preventDefault();
 
       // Grabbed values from text boxes
-      name = $("#name-input").val().trim();
-      destination = $("#destination-input").val().trim();
-      frequency = $("#frequency-input").val().trim();
-      // var convertedDate = moment(startDate, randomFormat);
-      // monthsWorked = (convertedDate).diff(moment(), "months")
-      first = $("#first-train-input").val().trim();
+      var name = $("#name-input").val().trim();
+      var destination = $("#destination-input").val().trim();
+      var frequency = $("#frequency-input").val().trim();
+      var first = $("#first-train-input").val().trim();
 
- 
-
-
-
-
-      // Code for handling the push into firebase
-      database.ref().push({
+      var addedTrain = {
         name: name,
         destination: destination,
         first: first,
         frequency: frequency,
-        next: next,
-        minAway: minAway,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+      };
+
+
+      // Code for handling the push into firebase
+      database.ref().push(addedTrain);
+
+          //clear text-boxes
+      $("#name-input").val("");
+      $("#destination-input").val("");
+      $("#first-train-input").val("");
+      $("#frequency-input").val("");
 
     });
 
-    // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.val();
 
-      // Console.loging the last user's data
-      console.log(sv.name);
-      console.log(sv.role);
-      console.log(sv.startDate);
-      console.log(sv.rate);
+    // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      // storing the snapshot.val() in a variable for convenience
+
+      name = childSnapshot.val().name
+      destination = childSnapshot.val().destination
+      first = childSnapshot.val().first
+      frequency = childSnapshot.val().frequency
+
+
+      //calculate minutes until next train
+      var trainStart = moment(first, "HH:mm").subtract(1, "years");
+      
+
+      var currentTime = moment();
+      
+      
+      var timeDiff = moment().diff(moment(trainStart), "minutes");
+      
+
+      var tRemainder = timeDiff % frequency;
+     
+      var minForNext = frequency - tRemainder;
+      
+
+      //calculate time of next train
+      var nextTrain = moment().add(minForNext, "minutes").format("HH:mm");
+      
 
       // Change the HTML to reflect - single change
-      $("#trainList").append('<div id="addTrain"><span class="data">' + name + '</span><span class="data">' + destination + '</span><span class="data">' + frequency + '</span><span class="data">' + next + '</span><span class="data">' + minAway + '</span></div>');
+      $("#trainList").append('<div id="addTrain"><span class="data">' + name + '</span><span class="data">' + destination + '</span><span class="data">' + frequency + '</span><span class="data">' + nextTrain + '</span><span class="data">' + minForNext + '</span></div>');
       $("#trainList").append('<hr class="style-two">');
-
-
-
-
 
       // Handle the errors
     }, function(errorObject) {
